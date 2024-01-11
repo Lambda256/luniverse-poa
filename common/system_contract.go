3f3db98778dd7f27bc475155cfff9a7cca625072
leo-lambda256 <leo.lim@lambda256.io>
2024-01-11 19:16:17 +0900

    fix: typo

--- common/system_contract.go
+++ common/system_contract.go
@@ -8,8 +8,8 @@ var VirtualMinerAddress = "0x00000000000000000000000000000000000003e7" // 999
 //=============================================================================================
 // Addresses of system contracts
 
-//var MultiSigExecutorContractAddress = "0x00000000000000000000000000000000000001f3" // 499
-//var InitialAllocationContractAddress = "0x00000000000000000000000000000000000001f4" // 500
+// var MultiSigExecutorContractAddress = "0x00000000000000000000000000000000000001f3" // 499
+// var InitialAllocationContractAddress = "0x00000000000000000000000000000000000001f4" // 500
 var ConsensusContractAddress = "0x00000000000000000000000000000000000001f9"  // 505
 var PreStakingContractAddress = "0x00000000000000000000000000000000000001fa" // 506
 var StakingContractAddress = "0x00000000000000000000000000000000000001fb"    // 507
@@ -20,7 +20,7 @@ var StakingContractAddress = "0x00000000000000000000000000000000000001fb"    // 
 var BoardMemberContractAddress = "0x0000000000000000000000000000000000000213" // 531
 //=============================================================================================
 
-// Block rewrad information (This information is retrieved from contract)
+// Block reward information (This information is retrieved from contract)
 type RewardPool struct {
 	Addr  *string `json:"addr"`
 	Ratio *uint16 `json:"ratio"`
